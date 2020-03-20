57e7453fae815a2d209b8425bbf930d0cd97e9e4
Tatsiana <49600781+tatsiana-sender@users.noreply.github.com>
2020-03-20 23:54:35 +0300

    add decision array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length == 0 ) {
+        return 0;
+  } else {
+      minimum = array[0];
+      for (i = 1; i < array.length; i++) {
+          if (array[i] < minimum ) {
+              minimum = array[i];
+          }
+      }
+    return minimum;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0 ) {
+        return 0;
+  } else {
+      maximum = array[0];
+      for (i = 1; i < array.length; i++) {
+          if (array[i] > maximum ) {
+              maximum = array[i];
+          }
+      }
+    return maximum;
+  }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0 ) {
+        return 0;
+  } else {
+      var sum = array.reduce(function(prev, current){
+          return prev + current;
+      })
+      return sum / array.length;
+  }
 }
